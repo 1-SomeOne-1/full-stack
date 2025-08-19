4ec3b3a9114f91aee50b386e402b585472061dfe
unknown <aaabdulraafeh1.2@gmail.com>
2025-08-18 21:47:11 -0700

    first

--- python/index.py
+++ python/index.py
@@ -1,7 +1,34 @@
 print("hello python")
 print("desdkl")
 
-
-p=5
-p=7
+p = 5
+p = 7
 print(p)
+
+# name = input("Enter your name:")
+# print(f"Hello {name}")
+#first = int(input("Enter first number :"))
+#operand = input("Enter operand : ")
+#second = int(input("Enter second number : "))
+#def operation(first,second,operand):
+# if operand=='+':
+#  return first+second
+# elif operand=='-':
+#  return first-second
+# elif operand=='*':
+#  return first*second
+# elif operand=='/':
+#  return first/second
+# else:
+#  print("invalid Operand")
+
+#result= operation(first=5, second=10, operand='+')
+
+#print("Result=",result)
+def list(lit):
+ for x in lit: 
+  if x == 5:
+   print(x)
+
+li = [1,2,3,4,5,6,7,8,9]
+list(li)
